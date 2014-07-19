91c0fc36b653637e2482085784b1b26e172b1ae7
dvoraka <alen.dvorak@gmail.com>
2014-07-19 20:09:24 +0200

    Add tests

---
+++ tests/test_data.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+
+from __future__ import unicode_literals
+from __future__ import print_function
+
+import unittest
+import data
+
+
+class TestPoint(unittest.TestCase):
+
+    def setUp(self):
+        
+        pass
+
+    def test_test(self):
+        
+        self.assertEqual(True, True)
+
