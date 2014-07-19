21690caee2de02cbc10be41424d95c822f9e981e
Alen Dvorak <alen.dvorak@gmail.com>
2014-07-19 20:58:44 +0200

    Add test

--- tests/test_data.py
+++ tests/test_data.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from __future__ import print_function
 
 import unittest
+import math
+
 import data
 
 
@@ -16,4 +18,31 @@ class TestPoint(unittest.TestCase):
     def test_test(self):
         
         self.assertEqual(True, True)
+
+    def test_chunk_distance(self):
+
+        p1 = data.Point(0, 0, 0)
+        p2 = data.Point(0, 0, 0)
+        result = p1.chunk_distance(p2)
+        self.assertAlmostEqual(result, 0)
+
+        p1 = data.Point(0, 0, 0)
+        p2 = data.Point(0, 150, 0)
+        result = p1.chunk_distance(p2)
+        self.assertAlmostEqual(result, 0)
+
+        p1 = data.Point(-10, 0, 0)
+        p2 = data.Point(10, 0, 0)
+        result = p1.chunk_distance(p2)
+        self.assertAlmostEqual(result, 20)
+
+        p1 = data.Point(0, 0, 10)
+        p2 = data.Point(0, 0, -10)
+        result = p1.chunk_distance(p2)
+        self.assertAlmostEqual(result, 20)
+
+        p1 = data.Point(-10, 0, 10)
+        p2 = data.Point(10, 0, -10)
+        result = p1.chunk_distance(p2)
+        self.assertAlmostEqual(result, math.sqrt(pow(20, 2) + pow(-20, 2)))
 
