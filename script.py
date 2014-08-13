5aeccaf3aa038eaed8df80ad1afe216440dcb47f
dvoraka <alen.dvorak@gmail.com>
2014-08-13 21:37:02 +0200

    Improve script module

--- script.py
+++ script.py
@@ -1,5 +1,7 @@
 from __future__ import print_function
 
+import re
+
 
 class Script(object):
 
@@ -27,3 +29,40 @@ class ScriptState(object):
     def process(self, context):
 
         pass
+
+
+class TokenizerException(Exception):
+
+    pass
+
+
+class Tokenizer(object):
+    """Class for token tools."""
+
+    def __init__(self, pattern):
+
+        self.token_pattern = pattern
+        self.token_re = re.compile(self.token_pattern, re.VERBOSE)
+
+    def tokenize(self, text):
+
+        position = 0
+        while True:
+
+            match_obj = self.token_re.match(text, position)
+
+            if not match_obj:
+
+                break
+
+            position = match_obj.end()
+
+            token_name = match_obj.lastgroup
+            token_value = match_obj.group(token_name)
+
+            yield token_name, token_value
+
+        if position != len(text):
+
+            raise TokenizerException(
+                "Tokenizer stopped at pos {} of {}".format(position, len(text)))
