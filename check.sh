f849205f3f110bcec99f11e39c68c3fddab7c6ac
dvoraka <alen.dvorak@gmail.com>
2014-09-07 16:57:57 +0200

    Improve code checker

--- check.sh
+++ check.sh
@@ -5,13 +5,16 @@
 
 MODULES="camera.py controls.py core.py data.py graphics.py player.py script.py shaders.py"
 CHECKER="flake8"
+PARAMS="--max-complexity 12"
 
 if ! hash ${CHECKER} 2>/dev/null
 then
-    echo "You need install ${CHECKER} program."
+    echo "You need to install ${CHECKER} program."
+    echo "For example:"
+    echo "\t\$ pip install ${CHECKER}"
     exit 1
 fi
 
-${CHECKER} ${MODULES}
+${CHECKER} ${PARAMS} ${MODULES}
 
-exit 0
+exit $?
