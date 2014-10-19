43b1ed7a049941441978bccb35882a3d6d819e23
dvoraka <alen.dvorak@gmail.com>
2014-10-19 20:08:57 +0200

    Add interfaces module (close #25)

---
+++ interfaces.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""Module for interfaces."""
+
+from __future__ import print_function
+
+
+class Creator(object):
+
+    def create(self):
+
+        raise NotImplementedError
+
+    def update(self):
+
+        raise NotImplementedError
