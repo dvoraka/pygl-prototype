2a48913e6e132077b926e7d1c888d30883e6a772
dvoraka <alen.dvorak@gmail.com>
2014-10-26 00:36:30 +0200

    Update layout checker

--- check.sh
+++ check.sh
@@ -3,7 +3,19 @@
 # Code layout checker
 #
 
-MODULES="camera.py controls.py core.py data.py graphics.py player.py script.py shaders.py"
+MODULES="
+    camera.py
+    configuration.py
+    controls.py
+    core.py
+    data.py
+    decorators.py
+    functions.py
+    graphics.py
+    interfaces.py
+    player.py
+    script.py
+    shaders.py"
 CHECKER="flake8"
 PARAMS="--max-complexity 12"
 
